341f16c816da23eaaad82169a4edb91d4a37bf13
hansrwindhoff <hansrwindhoff@gmail.com>
2014-06-14 13:21:24 -0600

    t

--- 01 interfaces.js
+++
@@ -1,3 +0,0 @@
-var v1;
-
-console.log('end');
--- 07 enum.js
+++
@@ -1,6 +0,0 @@
-var CustomersType;
-(function (CustomersType) {
-    CustomersType[CustomersType["Deadbeat"] = 0] = "Deadbeat";
-    CustomersType[CustomersType["Standard"] = 1] = "Standard";
-    CustomersType[CustomersType["Premium"] = 2] = "Premium";
-})(CustomersType || (CustomersType = {}));
