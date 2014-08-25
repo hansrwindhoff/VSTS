f2953f60e163d666c4d3cea5a981b08f0d442d9a
hansrwindhoff <hansrwindhoff@gmail.com>
2014-08-25 16:14:58 -0600

    vccs

--- 04 inference.ts
+++ 04 inference.ts
@@ -1,4 +1,4 @@
-// Example : inference with generics 
+// Example : inference with generics
 // lets define some interfaces
 //... a base interface
 interface ImybaseObj {
@@ -10,13 +10,13 @@ interface ImyextendedObj extends ImybaseObj {
 }
 
 // interface ItestedCondition{
-//   label:string; 
+//   label:string;
 //   condition:boolean
 // }
 
-//and one that derives from the first derived 
+//and one that derives from the first derived
 interface I2ndmyextendedObj extends ImyextendedObj  {
-  sumOverAGreaterZero:boolean;
+  sumOverA GreaterZero:boolean;
   //testedCondition: ItestedCondition;
 }
 
@@ -29,11 +29,11 @@ interface I2ndmyextendedObj extends ImyextendedObj  {
 
 
 // now we define two function interfaces for  functions
-// that will consume objects having the above interfaces , but only these 
+// that will consume objects having the above interfaces , but only these
 // or possibly other interfaces that are at least derived from the base, a constraint
 // a function that takes an input parameter of type T and
 // a call back that itselft takes the same T and returns a bool
-// the function return a T 
+// the function return a T
 //interface Ifct2<T extends ImybaseObj> {
 //  (inputPara: T, cb: (d: T) => boolean): T;
 //}
@@ -43,7 +43,7 @@ interface I2ndmyextendedObj extends ImyextendedObj  {
 interface Ifct3<T extends ImybaseObj, U extends ImybaseObj> {
   (inputPara: T, cb: (d: T) => boolean): U;
   //(inputPara: T, cb: (d: T) => ItestedCondition): U;
-  
+
 }
 
 
@@ -61,11 +61,11 @@ var w = { a: [2, 3, 4] };
 // console.log("u is a "+ typeof u);
 // console.log(u);
 
-// and a function that implements Ifct3 
+// and a function that implements Ifct3
 // so here we type the var t3
-var t3: Ifct3<ImyextendedObj, I2ndmyextendedObj>; //ts knows that the type parameters are constraint 
-                                                  //to the base type interface, 
- //var t3: Ifct3<number[], I2ndmyextendedObj>; // try this 
+var t3: Ifct3<ImyextendedObj, I2ndmyextendedObj>; //ts knows that the type parameters are constraint
+                                                  //to the base type interface,
+ //var t3: Ifct3<number[], I2ndmyextendedObj>; // try this
 
 // and because it is typed already ts knows that the function that is being assigned to t3
 // has the above types
@@ -76,14 +76,14 @@ t3 = (d, cb) => {
   return ret;
 };
 
-//ts knows that v is constraint to the base type interface, 
+//ts knows that v is constraint to the base type interface,
 //try putting it to w, that doesnt work,  because w is not of type ImyextendedObj
 //try putting {d:[1,2,3,4]} which doesnt fullfill the type requirement
 //but u works
 // ... now we are calling it
 var x =  t3(u, (d) =>{// is the sum over the array a greater zero?
   var sumoverarry = 0;
-    
+
   sumoverarry= d.a.reduce((prv,cur,i, theAr) => prv+cur); // try changing a => c
   console.log(sumoverarry);
   var ret =  sumoverarry > 0 ? true : false;
@@ -95,16 +95,16 @@ var x =  t3(u, (d) =>{// is the sum over the array a greater zero?
 
 console.log(x);
 
- 
+
 // // another axample for an implementation and the need to refactor
 // //var y = t3(w, function (d) {
 // var y = t3(u, function (d) {
 //  var ret;
 //  //d.b[0] > 0 ? ret = true : null; //w has no b
 //  ret = d.a[0] > 0 ? true : null; //this works
-//  return ret;// ret is any here 
+//  return ret;// ret is any here
 //  //return {label:"firstelementGreaterZero", condition:ret};
 // });
-// console.log(y); 
+// console.log(y);
 
 console.log('end');
